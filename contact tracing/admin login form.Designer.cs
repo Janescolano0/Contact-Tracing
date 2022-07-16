91e38dc465171fc1f24717284571283e3d627a01
Janescolano0 <joyann381@gmail.com>
2022-07-17 03:45:18 +0800

    added new form for admin login

---
+++ contact tracing/admin login form.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace contact_tracing
+{
+    partial class admin_login_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // admin_login_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(19)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "admin_login_form";
+            this.Text = "Admin LOGIN ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
